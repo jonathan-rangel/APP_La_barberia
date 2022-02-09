aa653e4a940dd1c868d7d583485ceb0609991ab5
Jonathan Rangel <jonathan.rangelrada@gmail.com>
2022-02-08 19:27:48 -0600

    Fix the final app

--- includes/app.php
+++ includes/app.php
@@ -8,6 +8,6 @@ $dotenv->safeLoad();
 require 'funciones.php';
 require 'database.php';
 
-// Conectarnos a la base de dat os
+// Conectarnos a la base de datos
 use Model\ActiveRecord;
 ActiveRecord::setDB($db);
